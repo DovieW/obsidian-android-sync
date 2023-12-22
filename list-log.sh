236abba56bbe290511b0f8af085fb2e03e6dd7f9
Dov Weinstock <DovieWeinstock@gmail.com>
2023-12-21 23:50:18 -0500

    allow reloading sync log

--- list-log.sh
+++ list-log.sh
@@ -3,11 +3,26 @@ source /data/data/com.termux/files/usr/etc/bash.bashrc
 
 log="$HOME/sync.log"
 
-if [[ -f $log ]]; then
-    cat $HOME/sync.log
-else
-    echo "No log yet..."
-fi
+# Start an infinite loop
+while true; do
+    clear # Clear the screen
 
-echo -e '\n\033[44;97mPress enter to finish...\033[0m'
-read none
+    # Check if log file exists
+    if [[ -f $log ]]; then
+        \cat $HOME/sync.log
+    else
+        echo "No log yet..."
+    fi
+
+    # Prompt the user
+    echo -e '\n\033[44;97mPress R to reload or Enter to exit...\033[0m'
+    read -r -n 1 -s input # Read a single character in silent mode
+
+    # Check if the input is Enter (empty)
+    if [[ -z $input ]]; then
+        break # Exit the loop (and the script)
+    elif [[ $input == "r" ]] || [[ $input == "R" ]]; then
+        continue # Continue the loop (reload)
+    fi
+done
+
